859147aa8aaefaaf3a1f22c50bbcd9655430d238
Harrison Mamin <hmamin@goguardian.com>
2019-10-11 16:18:05 -0700

    functionality for diff modes

--- portus.py
+++ portus.py
@@ -7,18 +7,38 @@ from pathlib import Path
 
 @click.command(context_settings={'help_option_names': ['--help', '-h']})
 @click.argument('file')
-@click.option('--overwrite', '-o', required=False, is_flag=True, default=False)
-@click.option('--auto', '-a', required=False, is_flag=True, default=False)
-@click.option('--destination', '-d', required=False, default=None)
-def portus(file, overwrite, auto, destination):
+@click.option('--overwrite', '-o', required=False, is_flag=True, default=False,
+              help="Boolean flag (default False). If passed, it will overwrite"
+                   "the existing output file if one exists. Without the flag, "
+                   "code will be appended to the end of the file.")
+@click.option('--mode', '-m', type=click.Choice(['p', 'a', 'f', 'm']),
+              required=False, default='p',
+              help="Mode that determines what kind of parsing is done. 'p' is "
+                   "for port mode, which only exports cells prefixed by the "
+                   "# PORT command. 'a' is for all, which parses all code "
+                   "cells. 'f' is for function mode, which only parses cells "
+                   "containing functions and classes. 'm' is for main mode "
+                   "which parses non-function/class cells (i.e. code that "
+                   "might be run if __name__ == '__main__').")
+@click.option('--dest_utils', '-du', required=False, default=None,
+              help='Output file to save functions to. If not specified, it '
+                   'will be the same as the notebook name except with '
+                   '"_utils.py" replacing ".ipynb".')
+@click.option('--dest_main', '-dm', required=False, default=None,
+              help='Output file to save non-function code to. If not '
+                   'specified, it will be the same as the notebook name except'
+                   ' with a .py extension.')
+def portus(file, overwrite, mode, dest_utils, dest_main):
     """Define a group of commands."""
     with open(file, 'r') as f:
         cells = [cell['source'] for cell in json.load(f)['cells']
                  if cell['cell_type'] == 'code' and cell['source']]
-    if auto:
-        files = parse_functions(file, cells, destination)
-    else:
+
+    # Populate dict mapping filename to code chunks.
+    if mode == 'p':
         files = parse_cells(cells)
+    else:
+        files = parse_auto(file, mode, cells, dest_utils, dest_main)
     write_files(files, overwrite)
 
 
@@ -50,7 +70,7 @@ def parse_cells(cells):
     return files
 
 
-def parse_functions(file, cells, destination):
+def parse_auto(file, mode, cells, dest_utils, dest_main):
     """
     Parameters
     -----------
@@ -62,11 +82,18 @@ def parse_functions(file, cells, destination):
     --------
     dict[str, list]: Maps filename to
     """
-    dest = destination or file.rstrip('ipynb') + 'py'
-    files = {dest: []}
+    dest_utils = dest_utils or f"{file.rstrip('.ipynb')}_utils.py"
+    dest_main = dest_main or f"{file.rstrip('.ipynb')}.py"
+
+    files = {dest_utils: [],
+             dest_main: []}
     for cell in cells:
-        if cell[0].startswith('def ') or cell[0].startswith('class '):
-            files[dest].append(''.join(cell))
+        is_func = cell[0].startswith('def ') or cell[0].startswith('class ')
+        chunk = ''.join(cell)
+        if mode in {'a', 'f'} and is_func:
+            files[dest_utils].append(chunk)
+        elif mode in {'a', 'm'} and not is_func:
+            files[dest_main].append(chunk)
     return files
 
 
